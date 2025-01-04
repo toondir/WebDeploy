21f155c5d292c4743c19c660597c2a4149d14e01
MATUPOOM SOONTORNTHANON <matumc@hotmail.com>
2025-01-04 03:05:29 -0600

    remove nav

---
+++ frontend/src/components/Navbar1.jsx
@@ -0,0 +1,10 @@
+
+function Navbar1() {
+  return (
+    <nav className="navbar1">
+      <h1>Navbar1</h1>
+    </nav>
+  );
+}
+
+export default Navbar1;
--- frontend/src/pages/Gallery.jsx
+++ frontend/src/pages/Gallery.jsx
@@ -1,4 +1,3 @@
-import Navbar from "../components/navbar";
 import React, { useState, useEffect,useRef } from 'react';
 import axios from 'axios';
 
@@ -9,8 +8,7 @@ import { GoogleMap, LoadScript, Marker, useLoadScript } from '@react-google-maps
 
 import PlacesAutocomplete from "../components/PlacesAutocomplete";
 
-import { Disclosure } from '@headlessui/react'
-import { NavLink } from 'react-router-dom'
+
 "use client";
 
 
@@ -323,7 +321,7 @@ setVisitedCountriesCount(filteredCountries.length);
   if (isLoading || !API_KEY) {
     return (
       <>
-        <Navbar />
+       
         <div className="flex justify-center items-center h-screen">
           <div className="animate-spin rounded-full h-16 w-16 border-t-2 border-b-2 border-blue-500"></div>
         </div>
@@ -335,7 +333,7 @@ setVisitedCountriesCount(filteredCountries.length);
    return(
     
     <>
-    <Navbar></Navbar>
+    
 
     <h1  className=" text-6xl font-medium mb-9 mt-9 text-center font-custom text-transparent bg-clip-text bg-gradient-to-r from-yellow-200 via-orange-500 to-orange-00">Gallery</h1>
     <h1 className="text-gray-400 text-4xl ml-6  font-custom">You have visited {visitedCountriesCount} countries</h1>
